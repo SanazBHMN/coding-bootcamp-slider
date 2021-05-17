39b20f4d173c4e9c84004173f42904d4162c8973
Sanaz Bahmani <sanazbahmani35@gmail.com>
2021-05-17 20:12:24 +0430

    Perfection

--- script/script.js
+++ script/script.js
@@ -1,7 +1,49 @@
-var slides = document.querySelector('.slider-items').children;
+// var slides = document.querySelector(".slider-items").children;
+var slides = [
+  {
+    image: "../images/image-tanya.jpg",
+    caption:
+      "“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m nowin the job of my dreams and so excited about the future. ”",
+    username: "Tanya Sinclair",
+    skill: "UX Engineer",
+  },
+  {
+    image: "../images/image-john.jpg",
+    caption:
+      " “ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”",
+    username: "John Smith",
+    skill: "Junior Frontend Developer",
+  },
+  {
+    image: "../images/image-john.jpg",
+    caption:
+      " “ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”",
+    username: "Asghar",
+    skill: "Junior Frontend Developer",
+  },
+  {
+    image: "../images/image-john.jpg",
+    caption:
+      " “ If you want to lay the best foundation possible I’d recommend taking this course. . I now feel so confident about starting up as a professional developer. ”",
+    username: "Akram",
+    skill: "Junior Frontend Developer",
+  },
+  {
+    image: "../images/image-john.jpg",
+    caption:
+      " “ The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”",
+    username: "Soghra",
+    skill: "Junior Frontend Developer",
+  },
+];
+var image = document.querySelector("img");
+var caption = document.querySelector("p");
+var username = document.querySelector(".name");
+var skill = document.querySelector(".skill");
+
 // console.log(slides)
-var nextSlide = document.querySelector('.right-slide');
-var prevSlide = document.querySelector('.left-slide');
+var nextSlide = document.querySelector(".right-slide");
+var prevSlide = document.querySelector(".left-slide");
 
 var totalSlides = slides.length;
 // console.log(totalSlides);
@@ -10,33 +52,29 @@ var index = 0;
 //      console.log('click');
 // });
 
-nextSlide.onclick = function() {
-      // console.log('click');
-      next('next');
+nextSlide.onclick = function () {
+  // console.log('click');
+  navigateSlider("next");
+};
+
+prevSlide.onclick = function () {
+  // console.log('prev click')
+  navigateSlider("prev");
+};
+
+function loadItem(index) {
+  data = slides[index];
+  image.src = data.image;
+  caption.innerText = data.caption;
+  username.innerText = data.username;
+  skill.innerText = data.skill;
 }
 
-prevSlide.onclick = function() {
-      // console.log('prev click')
-      next('prev');
+function navigateSlider(direction) {
+  index += direction === "next" ? 1 : -1;
+  if (index === -1) index = totalSlides - 1;
+  if (index === totalSlides) index = 0;
+  loadItem(index);
 }
 
-function next(direction) {
-      if(direction === 'next') {
-            index++;
-            if(index === totalSlides) {
-                  index = 0;
-            }
-      } else {
-           if(index == 0) {
-                 index = totalSlides - 1;
-           } else {
-                 index--;
-           }
-      }
-
-      for(var i = 0; i< slides.length; i++) {
-            slides[i].classList.remove('active');
-      }
-
-      slides[index].classList.add('active');
-}
+loadItem(0);
